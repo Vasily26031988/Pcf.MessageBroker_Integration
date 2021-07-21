08934d6d0f2d26c66fbe013e96da41bb4066c172
Vasily Gololobov <Sator1988@gmail.com>
2021-07-21 13:47:12 +0400

    Added files

---
+++ Pcf.ReceivingFromPartner.UnitTests/WebHost/Controllers/Partners/CancelPartnerPromoCodeLimitAsyncTests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pcf.ReceivingFromPartner.UnitTests.WebHost.Controllers.Partners
+{
+    public class CancelPartnerPromoCodeLimitAsyncTests
+    {
+        
+    }
+}
